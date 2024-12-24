d6f5df8d525aa5a6918c24e9e35a71c787868813
bas tian <bastian.bt66@gmail.com>
2024-12-24 07:43:07 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+# Copyright StackHPC Ltd. All Rights Reserved.
+# GNU General Public License v3.0+ (see COPYING or
+# https://www.gnu.org/licenses/gpl-3.0.txt)
+
+import setuptools
+
+setuptools.setup(
+    setup_requires=['pbr>=2.0.0'],
+    py_modules=[],
+    pbr=True)
